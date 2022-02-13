30c81f7b01ed3e8c2e9d8462b299d6780c0e6d27
Yuta Hoshino <y.hoshino25@gmail.com>
2022-02-14 01:01:42 +0900

    Add Posts showcase

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -4,9 +4,17 @@ import * as api from '../api';
 export const getPosts = ()=> async(dispath) =>{
     try {
         const { data } = await api.fetchPosts();
-
         dispath({ type:'FETCH_ALL', payload: data });
     } catch(error) {
         console.log(error.message);
     }
+}
+
+export const createPost = (post) => async(dispath) => {
+  try {
+      const { data } = await api.createPost(post);
+      dispath({ type:'CREATE', payload: data})
+  } catch(error) {
+      console.log(error);
+  }
 }
